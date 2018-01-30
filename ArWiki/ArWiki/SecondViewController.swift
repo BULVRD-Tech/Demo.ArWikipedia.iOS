461dec296aeca32d978bd466a0c79441114e3058
David Hodge <dhodge.jr229@gmail.com>
2018-01-29 23:06:42 -0500

    Initial Commit

---
+++ ArWiki/ArWiki/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  ArWiki
+//
+//  Created by David Hodge on 1/27/18.
+//  Copyright © 2018 David Hodge. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
